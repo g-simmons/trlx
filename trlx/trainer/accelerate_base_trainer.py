06404ecdeadec7b79293c8ff7773987fab54a558
Vlad Savinov <vlad.al.savinov@gmail.com>
2023-07-13 02:02:29 +0400

    Set gradient_accumulation_steps directly

--- trlx/trainer/accelerate_base_trainer.py
+++ trlx/trainer/accelerate_base_trainer.py
@@ -58,7 +58,12 @@ class AccelerateRLTrainer(BaseRLTrainer):
             self.mb_size = config.train.batch_size
         self.num_mb = config.train.batch_size // self.mb_size
         self.mb_count = 0
-        self.accelerator = Accelerator(log_with=config.train.tracker, project_dir=config.train.logging_dir, deepspeed_plugin=DeepSpeedPlugin(hf_ds_config=hf_ds_config))
+	deepspeed_plugin = DeepSpeedPlugin(hf_ds_config=hf_ds_config)
+	gradient_accumulation_steps = deepspeed_plugin.deepspeed_config["gradient_accumulation_steps"]
+        self.accelerator = Accelerator(
+		log_with=config.train.tracker, project_dir=config.train.logging_dir, 
+		deepspeed_plugin=deepspeed_plugin, gradient_accumulation_steps=gradient_accumulation_steps,
+	)
 
         if self.accelerator.state.deepspeed_plugin is not None:
             # by accelerate's default, arguments in `model.forward` would be casted to half
